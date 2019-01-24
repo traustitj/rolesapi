464a25270d4e955663ab5ce6df40ede1de404359
Trausti Thor Johannsson <traustitj@mac.com>
2019-01-25 00:04:10 +0100

    adding more test cases

--- server_service.py
+++ server_service.py
@@ -13,17 +13,48 @@ class Service:
         arr = mydb.test()
         return json.dumps(arr)
 
-    def get_user(self, userid):
+    def get_user_by_id(self, userid):
         mydb = Database()
-        user = mydb.get_user(userid)
+        user = mydb.get_user_by_id(userid)
         if not user is None:
-            ret = json.dumps("")
-            return "From database %s - %s - %d - %d\n" % (user.username, user.name, user.user_id, user.created)
+            return user
         
         returned = requests.get('http://tempo-test.herokuapp.com/7d1d085e-dbee-4483-aa29-ca033ccae1e4/1/user/%s/' % (userid))
         user = json.loads(returned.text)
         user = mydb.save_user(user)
-        return "returned from web: %s - %s - %d - %d\n" % (user.username, user.name, user.user_id, user.created)
+        return user
+
+    def get_role_by_id(self, role_id):
+        mydb = Database()
+        return mydb.get_role_by_id(role_id)
 
     def new_role(self, role_name):
-        pass
+        mydb = Database()
+        return mydb.create_role(role_name)
+
+    def delete_role_by_name(self, role_name):
+        mydb = Database()
+        return mydb.delete_role(role_name)
+
+    def add_user_to_role(self, user_id=0, role_id=0):
+        mydb = Database()
+        user = mydb.get_user_by_id(user_id)
+        role = mydb.get_role_by_id(role_id)
+
+        if user == None or role == None:
+            return False
+
+        if self.user_part_of_role(role.role_id):
+            return True
+
+        user = self.get_user_by_id(user_id)
+        role = self.get_role_by_id(role_id)
+
+        if user == None or role == None:
+            return False
+
+        return mydb.add_user_to_role(user_id, role_id)
+
+    def user_part_of_role(self, user_id=0, role_id=0):
+        mydb = Database()
+        return mydb.is_user_part_of_role(user_id=user_id, role_id=role_id)
